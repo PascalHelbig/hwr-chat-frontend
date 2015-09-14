9242920cfb6fccfe5d4c6967c8ee27feb72d7161
Tommy90 <ajsdakj@hdhd.de>
2015-09-14 21:00:50 +0200

    contacts view auf angulat material umstellen und noch ein wenig was am Layout in der Chatview ausprobiert. Habe mich da an telegram orientiert. So bekommen wir dann später auch die emojis usw besser unter         close #25

--- app/scripts/controllers/contacts.js
+++ app/scripts/controllers/contacts.js
@@ -8,7 +8,7 @@
  * Controller of the hwrChatApp
  */
 angular.module('hwrChatApp')
-  .controller('ContactsCtrl', function ($scope, screenService, $mdSidenav, $interval, userService) {
+  .controller('ContactsCtrl', function ($scope, screenService, $mdSidenav, $interval, userService, $state) {
     $scope.user = userService.me();
     $scope.chats = [];
     function getChats() {
@@ -21,6 +21,10 @@ angular.module('hwrChatApp')
 
     $scope.openSideNav = function() {
       $mdSidenav('left').toggle();
+    };
+
+    $scope.openChat = function(data){
+      $state.go('layout_2screens.chat', {id: data});
     };
 
     $scope.logout = function() {
