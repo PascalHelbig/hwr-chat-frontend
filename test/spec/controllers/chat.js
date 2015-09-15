936a861e6d78454f72a54a814a267b6e3a6336a7
Pascal Helbig <pascalhelbig@gmail.com>
2015-09-15 11:20:07 +0200

    Karma Test bereinigt

--- test/spec/controllers/chat.js
+++ test/spec/controllers/chat.js
@@ -16,17 +16,4 @@ describe('Controller: ChatCtrl', function () {
     });
   }));
 
-  it('should increase the chat array on send', function () {
-    var oldLength = scope.chat.length;
-    scope.messageText = 'Text';
-    scope.send();
-    expect(scope.chat.length).toBe(oldLength + 1);
-  });
-
-  it('should not increase the chat array if input is empty', function () {
-    var oldLength = scope.chat.length;
-    scope.messageText = '';
-    scope.send();
-    expect(scope.chat.length).toBe(oldLength);
-  });
 });
