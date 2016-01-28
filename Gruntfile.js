ac6033b83dd43893e567e25e071752c946ff1f88
pascalhelbig <pascalhelbig@gmail.com>
2016-01-28 11:42:06 +0100

    Sprachdateien beim Buildprozess mit einbinden

--- Gruntfile.js
+++ Gruntfile.js
@@ -342,7 +342,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'lang/{,*/}*.*'
           ]
         }, {
           expand: true,
